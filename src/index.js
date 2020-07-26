4796cfc80ec9f33e217462c4c0833e32c6c42394
Nikita Ulko <ulkenauer@gmail.com>
2020-07-27 02:31:35 +0700

    Split docs in parts. Composer fullSpec from parts with _.merge.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import SwaggerUI from 'swagger-ui'
 import 'swagger-ui/dist/swagger-ui.css';
 
+const _ = require('lodash')
+
 const spec = require('./swagger-config.yaml');
+const auth = require('./parts/auth.yaml');
+const user = require('./parts/user.yaml');
+const commonDataTypes = require('./parts/common-data-types.yaml');
+
+const fullSpec = _.merge({}, spec, auth, user, commonDataTypes)
 
 const ui = SwaggerUI({
-  spec,
+  spec: fullSpec,
   dom_id: '#swagger',
 });
 
